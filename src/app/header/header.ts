bbbd16be7ea9cd5e92ed2574f89654f6cb86c06e
ronaldo-ribeirof <filhoronaldo.ribeiro@gmail.com>
2026-07-23 10:18:31 -0300

    feat: update header imports

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { CartService } from '../cart/cart-service';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatBadgeModule],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
 
+  protected readonly cartService = inject(CartService);
 }
