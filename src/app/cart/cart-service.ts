84dd86a8ac2e182ac81469ebc9712e19fde87cb2
ronaldo-ribeirof <filhoronaldo.ribeiro@gmail.com>
2026-07-23 10:19:00 -0300

    refactor: updated the cart interfaces

--- src/app/cart/cart-service.ts
+++ src/app/cart/cart-service.ts
@@ -1,15 +1,28 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import { Product } from '../products/product';
+import { CartItem } from './cart-item';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
+  private readonly cartItems = signal<CartItem[]>([]);
 
-  private readonly cartItems = signal<Product[]>([]);
+  readonly totalItems = computed(() =>
+    this.cartItems().reduce((total, item) => total + item.quantity, 0),
+  );
 
   addToCart(product: Product) {
-    console.log('Added to cart: ', product.name);
-    this.cartItems.update((items) => [...items, product]);
+    this.cartItems.update((items) => {
+      const existingItem = items.find((item) => item.product.id === product.id);
+
+      if (existingItem) {
+        return items.map((item) =>
+          item.product.id === product.id ? { ...item, quantity: item.quantity + 1 } : item,
+        );
+      }
+
+      return [...items, { product, quantity: 1 }];
+    });
   }
 }
